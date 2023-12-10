7e1fa9d4f41d31f2c92c74cbf27636364742a561
GOUTHAMI SUVARNA <76654382+Gomigoku@users.noreply.github.com>
2023-12-10 18:20:49 +0530

    Create App.js

---
+++ progressBar/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import ProgressBar from "./components/ProgressBar";
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
+  }, []);
+
+  return (
+    <div className="App">
+      <span>Progress Bar</span>
+      <ProgressBar value={value} />
+    </div>
+  );
+}
+
+export default App;
