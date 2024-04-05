6b8511aacc64df1b30562e164ed0e1c4ab948b82
GOUTHAMI POOJARY <76654382+Gomigoku@users.noreply.github.com>
2024-04-05 09:21:45 +0530

    Update App.js

--- TextEditor/client/App.js
+++ TextEditor/client/App.js
@@ -1,9 +1,11 @@
-import TextEditor from "./TextEditor";
-function App() {
-  return (
-    <TextEditor />
-    
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css';
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
