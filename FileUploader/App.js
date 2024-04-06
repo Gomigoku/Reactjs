f45af6293f8a51e3fa8c9da5a9d8bc832ee5cf27
GOUTHAMI POOJARY <76654382+Gomigoku@users.noreply.github.com>
2024-04-06 21:11:18 +0530

    Create App.js

---
+++ FileUploader/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Upload a file</h1>
+      <input type="file"></input>
+      <button>Upload</button>
+    </div>
+  );
+}
+
+export default App;
