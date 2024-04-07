d9f0bf493615ddbe248ca53a6e616c53229b4d67
GOUTHAMI POOJARY <76654382+Gomigoku@users.noreply.github.com>
2024-04-07 18:47:12 +0530

    Update App.js

--- FileUploader/App.js
+++ FileUploader/App.js
@@ -1,13 +1,48 @@
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  const [selectedFile, setSelectedFile] = useState();
+
+  const onFileChange =(event) =>{
+      setSelectedFile(event.target.files[0]);
+  }
+
+  const onFileUpload = () =>{
+      const formData = new FormData();
+      formData.append("myFiles", selectedFile, selectedFile.name);
+      console.log(selectedFile);
+      console.log(formData);
+      axios.post("api/uploadfiles", formData);
+  }
+
+  const fileData = () => {
+    if(selectedFile){
+      return(
+        <div>
+          <h1>File Details:</h1>
+          <p>File Name: {selectedFile.name}</p>
+          <p>File Type: {selectedFile.type}</p>
+          <p>Last Modified: {selectedFile.lastModifiedDate.toDateString()}</p>
+        </div>
+      );
+    }
+    else{
+      return( <div>
+        <br></br>
+        <h4>Choose file before hitting upload button!!</h4>
+      </div>);
+    }
+  }
   return (
-    <div className="App">
-      <h1>Upload a file</h1>
-      <input type="file"></input>
-      <button>Upload</button>
+    <div>
+      <h1>File Uploader</h1>
+      <input type="file" onChange={onFileChange}></input>
+      <button onClick={onFileUpload}>Upload!</button>
+      {fileData()}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
