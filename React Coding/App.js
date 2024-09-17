7cf9c0b83f79b6bc47802c62dcc1b4e9e51c3794
GOUTHAMI POOJARY <76654382+Gomigoku@users.noreply.github.com>
2024-09-17 22:36:48 +0530

    Create App.js

---
+++ React Coding/App.js
@@ -0,0 +1,11 @@
+/** 
+  Challenge: Display `JSX is cool!` as an output
+  
+  Solution: https://codepen.io/angelo_jin/pen/xxXrZLd
+  Video for reference: https://youtu.be/VzNNjNmbXpY
+**/
+function App() {
+  return <div>JSX is cool!</div>
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
